c808d822289a27faade1a149cf6c1119986727df
MrCorvid <91694824+MrCorvid@users.noreply.github.com>
2025-08-23 14:06:17 -0700

    Add AST_Variable.cs

---
+++ 2. Parse and make AST/AST_Variable.cs
@@ -0,0 +1,17 @@
+// ProgrammingLanguageNr1/AST_Variable.cs
+
+namespace ProgrammingLanguageNr1
+{
+    public class AST_Variable : AST
+    {
+        public AST_Variable(Token token) : base(token)
+        {
+            // A variable usage is represented by its NAME token.
+        }
+
+        public override string ToString()
+        {
+            return "var:" + getTokenString();
+        }
+    }
+}
